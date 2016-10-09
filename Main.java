592253651c535350e23ef4ba205b68ed603c3926
anton <pomanitzz@gmail.com>
2016-10-09 23:48:33 +0300

    fixed the broken, add hot information

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ public class Main{
 
 	public static void main(String []args) {
 	
-	System.out.priintln("Updated information");	
+	System.out.priintln("Updated information");
+	System.out.println("Hot fix information, very hothothot");	
 		
 	} 
 }
