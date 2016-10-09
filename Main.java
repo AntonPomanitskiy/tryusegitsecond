6610355e5123f784b46b3fa496b06441dd7fc8fb
anton <pomanitzz@gmail.com>
2016-10-09 23:44:42 +0300

    my new updates when i work

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main{
 	public static void main(String []args) {
 	
 	System.out.priintln("Updated information");	
+	System.out.println("New updates, when i work with this project");
 		
 	} 
 }
