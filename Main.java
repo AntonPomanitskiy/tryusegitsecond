d875c9e9735371f9bbf6fdf8594c37d8d750b4c5
anton <pomanitzz@gmail.com>
2016-10-09 23:54:12 +0300

    add more information

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main{
 	
 	System.out.priintln("Updated information");	
 	System.out.println("New updates, when i work with this project");
+	System.out.println("Some more new update information while i work");
 		
 	} 
 }
